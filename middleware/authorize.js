90c4ed2abc488d033e9bd31794faf9a9bf379968
Kishor Langote <kishorlangoet2@gmail.com>
2025-06-30 08:11:18 +0530

    update auth file

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -3,6 +3,7 @@ const User = require("../models/user.model")
 
  const authenticate = async (req, res, next) => {
 const token = req.cookies.jwt
+console.log("token:", token)
 if(!token) {
   return res.status(401).json({ message: "Unauthorized"})
 }
@@ -10,6 +11,8 @@ if(!token) {
 try {
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
   req.user = await User.findById(decoded.userId)
+  console.log("user in request:", req.user);
+
 } catch (error) {
   console.log(error);
   res.status(500).json({ message: "" + error.message})
